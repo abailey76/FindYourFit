0c1f9902280f0276c3e24d912dbf4dea5082e0ed
Adam Bailey <abailey76@duck.com>
2022-01-16 13:14:53 -0600

    created giignore+base project w/ auth

---
+++ FindYourFit/FindYourFit/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FindYourFit.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FindYourFit.Areas.Identity.IdentityHostingStartup))]
+namespace FindYourFit.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ FindYourFit/FindYourFit/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FindYourFit.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
